6e7e908edd4ccf702a1d5f37510fc4e47d2830f5
guilherme-x <guilherme.kaliter@gmail.com>
2023-12-04 21:57:36 -0300

    BOILERPLATE

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import Login from './login';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* ADICIONE OS COMPONENTES QUE VOCÊS CRIAREM AQUI */}
     </div>
   );
 }
