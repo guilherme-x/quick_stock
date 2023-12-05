21ffce7752ab3169c1dcf6b7333eaf1b5ae24bdc
MillenaCastro <millenafarias14@gmail.com>
2023-12-05 20:06:39 -0300

    Listagem de produtos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './login';
+import ProductList from './products';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <ProductList/>
     </div>
   );
 }
