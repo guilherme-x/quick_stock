7d45b83080a81a1dce103ff2a95d43df10d4f336
guilherme-x <guilherme.kaliter@gmail.com>
2023-12-05 23:10:04 -0300

    Refactor routing in App.js and products/index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import Login from './login';
-import ProductList from './products';
 import Cadastro from './products/cadastro';
+import ProductRoutes from './products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <ProductList />
-      <Cadastro />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="products/*" element={<ProductRoutes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
